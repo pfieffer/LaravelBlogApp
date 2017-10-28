3433bd92eee8659aef7972e36324137dd11a89d0
RaviGarbujaPun <cbpmgr@gmail.com>
2017-10-28 16:00:54 +0545

    Implement delete Post functionality

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -114,6 +114,8 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'Post Deleted');
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,4 +13,10 @@
 
     <hr>
     <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+    {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+
+    {!! Form::close() !!}
 @endsection
